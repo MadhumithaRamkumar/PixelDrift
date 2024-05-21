bcdc8c719f30d6cc4f84f1dc6219feef7817bf72
MadhumithaRamkumar <d.madhumitha@gmail.com>
2024-05-21 20:04:38 +1200

    View user, Delete user,View Image,Edit user functionality added

--- PixelDrift/Controllers/HomeController.cs
+++ PixelDrift/Controllers/HomeController.cs
@@ -78,7 +78,7 @@ namespace PixelDrift.Controllers
             else
             {
 
-                System.Windows.Forms.MessageBox.Show("Incorrect User Name and Password");
+                System.Windows.Forms.MessageBox.Show("Incorrect User Name/Password");
                 return View();
             }
         }
@@ -101,13 +101,19 @@ namespace PixelDrift.Controllers
                     _dbContext.user_Logins.Add(u);
                     _dbContext.SaveChanges();
 
-                      
+                    System.Windows.Forms.MessageBox.Show("User Added Successfully");
+                    return RedirectToAction("Dashboard");
+
                 }
                     
             }
             return View(u);
         }
 
+        public ActionResult CreateUserFreshPage()
+        {
+            return View();
+        }
         public ActionResult Dashboard()
         {
             return View();
@@ -201,7 +207,7 @@ namespace PixelDrift.Controllers
                     ImageSave imagedetails = new ImageSave();
                     imagedetails.User_Id = (string)Session["User"];
                     imagedetails.FileName = image.FileName;
-                    _dbContext.ImageSave.Add(imagedetails);
+                    _dbContext.ImageSave.Remove(imagedetails);
                     _dbContext.SaveChanges();
 
 
@@ -209,6 +215,50 @@ namespace PixelDrift.Controllers
                 }
             return RedirectToAction("Delete");
         }
+
+        //View Images code
+
+        public ActionResult ViewImage()
+        {
+            string user_blob = (string)Session["User"];
+            CloudBlobContainer blobConatiner = _blobStorageService.GetCloudBlobContainer();
+            List<string> fileName = new List<string>();
+
+            fileName = _dbContext.ImageSave.Where(x => x.User_Id == user_blob).Select(p => p.FileName).ToList();
+
+
+            List<string> blobs = new List<string>();
+            foreach (var blobItem in blobConatiner.ListBlobs())
+            {
+                foreach (var name in fileName)
+                    if (blobItem.Uri.ToString().Contains(name))
+                        blobs.Add(blobItem.Uri.ToString());
+            }
+            return View(blobs);
+        }
+
+        [HttpPost]
+        public ActionResult ViewImage(HttpPostedFileBase image)
+        {
+
+
+
+            if (image != null)
+                if (image.ContentLength > 0)
+                {
+                    CloudBlobContainer blobContainer = _blobStorageService.GetCloudBlobContainer();
+                    CloudBlockBlob blob = blobContainer.GetBlockBlobReference(image.FileName);
+                    blob.UploadFromStream(image.InputStream);
+                    ImageSave imagedetails = new ImageSave();
+                    imagedetails.User_Id = (string)Session["User"];
+                 
+
+
+
+                }
+            return RedirectToAction("ViewImage");
+        }
+
         [HttpPost]
 
         public string   DeleteImage(String Name)
